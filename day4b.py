c8929653a5ebd185be212dc929a3bcf8d79d8c95
Jaime Liew <jaimeliew1@gmail.com>
2020-12-04 17:50:15 +0100

    change day4b to regex

--- day4b.py
+++ day4b.py
@@ -12,59 +12,21 @@ def extract_dictionary(x: str) -> dict:
     return out
 
 
-def is_number(x, vmin=None, vmax=None):
-    try:
-        x = int(x)
-
-        return vmin <= x <= vmax
-    except:
-        return False
-
-
-def height_valid(x):
-    unit = x[-2:]
-    val = x[:-2]
-    if unit not in ["cm", "in"]:
-        return False
-    if unit == "cm":
-        return 150 <= int(val) <= 193
-    elif unit == "in":
-        return 59 <= int(val) <= 76
-
-
-def hair_valid(x):
-    if x[0] != "#":
-        return False
-    return all(c in "abcdef0123456789" for c in x[1:])
-
-
-def valid_pid(x):
-    if len(x) != 9:
-        return False
-    try:
-        x = int(x)
-        return True
-    except:
-        return False
-
-
 def passport_valid(passport: dict) -> bool:
     required = {
-        "byr": lambda x: is_number(x, 1920, 2002),
-        "iyr": lambda x: is_number(x, 2010, 2020),
-        "eyr": lambda x: is_number(x, 2020, 2030),
-        "hgt": height_valid,
-        "hcl": hair_valid,
-        "ecl": lambda x: x in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"],
-        "pid": valid_pid,
+        "byr": "19[2-9][0-9]|200[0-2]",
+        "iyr": "201[0-9]|2020",
+        "eyr": "202[0-9]|2030",
+        "hgt": "1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in",
+        "hcl": "#[0-9a-f]{6}",
+        "ecl": "amb|blu|brn|gry|grn|hzl|oth",
+        "pid": "[0-9]{9}",
     }
     if not all(x in passport.keys() for x in required.keys()):
         return False
-    if all(func(passport[key]) for key, func in required.items()):
-        return True
-    else:
-        print(passport)
-        return False
+    return all(
+        re.fullmatch(pattern, passport[key]) for key, pattern in required.items()
+    )
 
 
 def main():
@@ -73,8 +35,8 @@ def main():
 
     count = 0
     for x in data.split("\n\n"):
-
         passport = extract_dictionary(x)
+
         if passport_valid(passport):
             count += 1
 
