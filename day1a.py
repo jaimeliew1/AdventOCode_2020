d323af7d24280cf1d3dce0953ad800fec0c03269
Jaime Liew <jaimeliew1@gmail.com>
2020-12-04 15:58:39 +0100

    first commit. Day 1.

---
+++ day1a.py
@@ -0,0 +1,14 @@
+from itertools import combinations
+
+def main():
+    data = []
+    with open("data/data_1.txt", "r") as f:
+        for line in f:
+            data.append(int(line))
+
+    for x, y in combinations(data, 2):
+        if x + y == 2020:
+            print(x, y, x * y)
+
+if __name__ == '__main__':
+    main()
---
+++ day1b.py
@@ -0,0 +1,14 @@
+from itertools import combinations
+
+def main():
+    data = []
+    with open("data/data_1.txt", "r") as f:
+        for line in f:
+            data.append(int(line))
+
+    for x, y, z in combinations(data, 3):
+        if x + y + z == 2020:
+            print(x, y, z, x * y * z)
+
+if __name__ == '__main__':
+    main()
